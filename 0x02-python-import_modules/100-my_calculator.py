c4c0670e706b69726f84ec8fea4e189aa305aba4
Thabani Takwena <thabanidev@gmail.com>
2024-03-15 20:59:49 +0200

    Fix import and remove unnecessary whitespace in my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,6 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-
 if __name__ == "__main__":
     num_of_args = len(sys.argv) - 1
     if (num_of_args != 3):
@@ -12,7 +11,7 @@ if __name__ == "__main__":
         a = int(sys.argv[1])
         operator = sys.argv[2]
         b = int(sys.argv[3])
-        
+
         if operator == '+':
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif operator == '-':
