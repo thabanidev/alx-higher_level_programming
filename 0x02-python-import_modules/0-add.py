38f14d2233584bf6f011a800a8366b3e66552e12
Thabani Takwena <thabanidev@gmail.com>
2024-03-15 14:08:53 +0200

    Refactor import statement in 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 
 if __name__ == '__main__':
     a = 1
     b = 2
-    print(f"{a} + {b} = {add_0.add(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
