5d00ff66be7966aaf69c1b1cf93d460add307a32
Thabani Takwena <thabanidev@gmail.com>
2024-04-09 09:43:06 +0200

    Refactor replace_in_list function to directly modify the list in 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    return my_list if 0 > idx >= len(my_list) else my_list[:idx] + [element] + my_list[idx + 1:]
+    if 0 > idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
