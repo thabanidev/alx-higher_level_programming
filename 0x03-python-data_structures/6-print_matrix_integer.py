0eb576d1fc01df6f366a849421bfaed4c62627db
Thabani Takwena <thabanidev@gmail.com>
2024-04-15 16:02:02 +0200

    Fix formatting issue and out-of-bounds index check in print_matrix_integer function

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None or matrix == [[]]:
-        print()
+    if matrix == [[]]:
+        return
     for row in matrix:
         for n in row:
             print("{:d}".format(n), end="")
+            if n != row[-1]:
+                print(" ", end="")
         print()
