2f564364762309df96af0f754a28f58d4143d9fd
Thabani Takwena <thabanidev@gmail.com>
2024-05-18 11:27:05 +0200

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/python3
+
+def multiple_returns(sentence):
+    if sentence == "":
+        new_tuple = (0, None)
+    else:
+        new_tuple = (len(sentence), sentence[0])
+    return new_tuple
