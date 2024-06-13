d0946554ac98d5438bdbdef26f2f443d57419c32
Thabani Takwena <thabanidev@gmail.com>
2024-06-13 14:34:31 +0200

    made line count shorter

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+def multiply_list_map(my_list=[], number=0): list(map(lambda x: x * number, my_list))
