ad2a47b3c6eee23901cdaaebfac4cebdc44cf3a5
Thabani Takwena <thabanidev@gmail.com>
2024-06-13 14:54:57 +0200

    test refactor

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,2 +1,2 @@
-#!/usr/bin/python3
-multiply_list_map = lambda my_list=[], number=0: list(map(lambda x: x * number, my_list or []))
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
