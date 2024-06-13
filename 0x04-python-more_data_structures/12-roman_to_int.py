f96c14202094286d01bd237f58ef66ae6d7f2bd8
Thabani Takwena <thabanidev@gmail.com>
2024-06-13 16:53:40 +0200

    roman to int interview prep task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    previous_value = 0
+    for current_numeral in reversed(roman_string):
+        current_value = roman_map[current_numeral]
+        if current_value >= previous_value:
+            result += current_value
+        else:
+            result -= current_value
+        previous_value = current_value
+    return result
