2be4c534d7a92658313f5c34990e4ab13f3315e9
Thabani Takwena <thabanidev@gmail.com>
2024-06-18 17:54:06 +0200

    REmoved whitespace on blank lines

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     """Print x elements of a list.
-    
     Args:
         my_list (list): List of elements to print.
         x (int): Number of elements to print.
-    
     Returns:
         int: Number of elements printed.
     """
