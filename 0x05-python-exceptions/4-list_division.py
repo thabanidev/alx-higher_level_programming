c776c36022d18dfc4312bc11f91729d7b8db1a8e
Thabani Takwena <thabanidev@gmail.com>
2024-06-18 19:23:39 +0200

    list division task

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides element by element of two lists
+
+    Args:
+        my_list_1 (list): list of numbers
+        my_list_2 (list): list of numbers
+        list_length (int): length of the lists
+
+    Returns:
+        list: list of size list_length with all division results
+    """
+    my_list = []
+    for i in range(list_length):
+        try:
+            my_list.append(my_list_1[i] / my_list_2[i])
+        except (ZeroDivisionError, TypeError, IndexError) as ex:
+            my_list.append(0)
+            if isinstance(ex, ZeroDivisionError):
+                print("division by zero")
+            elif isinstance(ex, TypeError):
+                print("wrong type")
+            elif isinstance(ex, IndexError):
+                print("out of range")
+    return my_list
