3ed47bf6db1ca5289e1854db0deb87ccc2fa6536
Thabani Takwena <thabanidev@gmail.com>
2024-06-18 22:42:28 +0200

    coordinate of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Defines a Square class with properties."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes a square.
+
+        Args:
+            size (int, optional): The size of the square.
+            position (tuple, optional): The position of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif int(size) < 0:
+            raise ValueError("size must be >= 0")
+        if not isinstance(position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size**2
+
+    @property
+    def size(self):
+        """Gets or sets the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif int(value) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Gets or sets the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise ValueError("position must be a tuple of 2 positive integers")
+        for i in value:
+            if not isinstance(i, int):
+                raise TypeError("position must be a tuple of 2 positive integers")
+            elif int(i) < 0:
+                raise ValueError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """Prints in stdout the square with '#' at the set position."""
+        if self.__size == 0:
+            print()
+        else:
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+                print("")
+                  
