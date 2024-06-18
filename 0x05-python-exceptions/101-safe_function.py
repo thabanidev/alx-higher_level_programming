f8e0834f9aa7ba311c497cb7e94c1ea9762e6b3f
Thabani Takwena <thabanidev@gmail.com>
2024-06-18 21:20:51 +0200

    change error output to stderr

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_function(fct, *args):
     result = None
     try:
         result = fct(*args)
     except Exception as ex:
-        print("Exception: {}".format(ex))
+        print("Exception: {}".format(ex), file=sys.stderr)
     return result
