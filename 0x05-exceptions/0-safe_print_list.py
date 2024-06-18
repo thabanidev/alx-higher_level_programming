b9f9fca094abea980b5d011a49936095cb02ebe8
Thabani Takwena <thabanidev@gmail.com>
2024-06-18 16:58:58 +0200

    safe print list task

---
+++ 0x05-exceptions/0-safe_print_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_list(my_list = [], x = 0):
+    """Safely prints out elements of a list
+
+    Args:
+        my_list (list, optional): List who's elements should be printed. Defaults to [].
+        x (int, optional): Number of elements to print. Defaults to 0.
+    """
+    if not my_list:
+        return 0.0
+    printed_count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+            printed_count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return printed_count
+    
