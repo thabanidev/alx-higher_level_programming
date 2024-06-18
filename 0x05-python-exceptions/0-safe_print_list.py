146267c422ce655885c0826cd27ea9067b503434
Thabani Takwena <thabanidev@gmail.com>
2024-06-18 17:22:51 +0200

    refactor

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,13 +5,17 @@ def safe_print_list(my_list = [], x = 0):
     Args:
         my_list (list, optional): List who's elements should be printed. Defaults to [].
         x (int, optional): Number of elements to print. Defaults to 0.
+    Returns:
+        int: Number of elements printed
     """
     if not my_list:
         return 0
     printed_count = 0
     try:
-        for i in range(x):
-            print(my_list[i], end="")
+        for i, item in enumerate(my_list):
+            if 0 <= i >= x:
+                raise IndexError
+            print(item, end="")
             printed_count += 1
     except IndexError:
         pass
