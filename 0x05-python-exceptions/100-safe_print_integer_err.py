7b201076669530b2bc684ab33793b2feae55818e
Thabani Takwena <thabanidev@gmail.com>
2024-06-18 21:13:42 +0200

    consider ValueError

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -14,6 +14,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError as ex:
+    except (TypeError, ValueError) as ex:
         print("Exception: {}".format(ex), file=sys.stderr)
         return False
