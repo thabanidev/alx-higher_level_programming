68fc9ce9d2218e45dfbfe71c542dbca997d3f5f2
Thabani Takwena <thabanidev@gmail.com>
2024-06-18 17:43:12 +0200

    Changed printing newline to outside loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -16,6 +16,5 @@ def safe_print_list(my_list = [], x = 0):
             number_of_elements_printed += 1
         except IndexError:
             break
-        finally:
-            print()
+    print()
     return number_of_elements_printed    
