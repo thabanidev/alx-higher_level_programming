a76ced7571a487657c1d1130ed679adbbb4d8125
Thabani Takwena <thabanidev@gmail.com>
2024-06-18 17:50:36 +0200

    removed whitespace

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -17,4 +17,4 @@ def safe_print_list(my_list = [], x = 0):
         except IndexError:
             break
     print()
-    return number_of_elements_printed    
+    return number_of_elements_printed
