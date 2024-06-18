e02b4f9071e7ff4a1eb6f5d84ebebe1bd1f03c62
Thabani Takwena <thabanidev@gmail.com>
2024-06-18 17:04:20 +0200

    changed end of line print output

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,7 @@ def safe_print_list(my_list = [], x = 0):
     printed_count = 0
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
             printed_count += 1
     except IndexError:
         pass
